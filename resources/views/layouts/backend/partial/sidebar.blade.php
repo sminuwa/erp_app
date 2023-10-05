f6ffe1e9f30f41416e0c3bf8f47001ee7a7619df
Sunusi M. Inuwa <sminuwa@yahoo.com>
2023-10-05 22:09:51 +0100

    asdfa

--- resources/views/layouts/backend/partial/sidebar.blade.php
+++ resources/views/layouts/backend/partial/sidebar.blade.php
@@ -195,13 +195,6 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#"
-                               class="nav-link ">
-                                <i class="ion-card"></i>
-                                <p>Expenses</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
                             <a href="{{route('interbank.list')}}"
                                class="nav-link ">
                                 <i class="ion-card"></i>

9b409f27aeb8bb652ea5de48eb74816b6add68f6
Sunusi M. Inuwa <sminuwa@yahoo.com>
2023-10-18 12:37:03 +0100

    asdfaf

--- resources/views/tables/purchase_request.blade.php
+++ resources/views/tables/purchase_request.blade.php
@@ -18,7 +18,7 @@
                 <td> {{ $record->invoice }} </td>
                 <td> {{ $record->purchase_date->toDayDateTimeString() }} </td>
                 <td style="text-align: right"> {{ number_format($record->totalProductCost()->total, 2) }} </td>
-                <td> {{ $record->status == 1 ? 'Completed' : 'Pending' }} </td>
+                <td> {{ $record->status == 1 ? 'Close' : 'Open' }} </td>
                 <td>
                     <a class="btn btn-secondary btn-sm" href="{{ route('purchases.request.show', $record->id) }}">
                         <span class="fa fa-eye"></span>

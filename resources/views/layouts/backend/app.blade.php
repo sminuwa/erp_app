75fc0b60f6f20e9a2aaff45deb8ae953af41d2d2
Sunusi M. Inuwa <sminuwa@yahoo.com>
2023-10-27 14:54:44 +0100

    asdfasdf

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('assets/backend/css/adminlte.min.css') }}">
     <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+{{--    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">--}}
 
     <link rel="stylesheet" href="{{ asset('assets/backend/css/toastr.min.css') }}">
 

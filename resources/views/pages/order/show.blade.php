86ad259a76cd6cc6f2cbc783f8f367865604e764
Sunusi M. Inuwa <sminuwa@yahoo.com>
2023-11-03 14:38:07 +0100

    asdfasdf

--- resources/views/pages/order/show.blade.php
+++ resources/views/pages/order/show.blade.php
@@ -121,8 +121,7 @@
                                 </div>
                                 <!-- /.col -->
                                 <div class="col-sm-4 invoice-col">
-                                    <b>Invoice
-                                        No: {{ $order->invoice_no }}</b><br><br>
+                                    <b>Reference No: {{ $order->reference }}</b><br><br>
                                     <b>Order Status:</b>
                                     {!!
                                         $order->status == 0 ? '<span class="badge badge-warning">Pending</span>':

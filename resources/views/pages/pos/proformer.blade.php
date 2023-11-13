cdb32f094deadda21e26a4c80b625fb7b8dad768
Sunusi M. Inuwa <sminuwa@yahoo.com>
2023-11-13 14:54:34 +0100

    asdfaf

--- resources/views/pages/pos/proformer.blade.php
+++ resources/views/pages/pos/proformer.blade.php
@@ -107,7 +107,7 @@
 
                         </div>
                     </div>
-                    <div class="col-md-6 cart-container">
+                    <div class="col-md-6 ">
                         <!-- general form elements -->
                         <div class="card">
                             <div class="card-header">

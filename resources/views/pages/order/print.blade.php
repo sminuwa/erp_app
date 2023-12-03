53e660454f6242aa4eadb7b311fc9ac10afd6cde
unknown <engrabusadik@gmail.com>
2023-12-04 03:58:06 +0800

    Worked

--- resources/views/pages/order/print.blade.php
+++ resources/views/pages/order/print.blade.php
@@ -46,9 +46,11 @@
                     <!-- title row -->
                     <div class="row">
                         <div class="col-11">
-                            <img src="{{ asset('assets/backend/img/logo'.App\Models\User::userBranchAction().".png") }}" style="width:80px;height:80px;"
-                                alt="logo" class="img-circle elevation-3" style="opacity: .8">
-                            <span style="font-size:24px;">&nbsp;{{App\Models\User::userBranchName()->long_name}}</span>
+                            <img src="{{ asset('assets/backend/img/logo' . App\Models\User::userBranchAction() . '.png') }}"
+                                style="width:80px;height:80px;" alt="logo" class="img-circle elevation-3"
+                                style="opacity: .8">
+                            <span
+                                style="font-size:24px;">&nbsp;{{ App\Models\User::userBranchName()->long_name }}</span>
 
                             <small class="float-right">Date: {{ date('l, d-M-Y h:i:s A') }}</small>
 
@@ -89,7 +91,8 @@
                     <div class="row">
                         <div class="col-sm-4 invoice-col">
                             <address>
-                                <b>Customer Name:</b> {{ $order->customer->code }} - {{ $order->customer->name }}<br />
+                                <b>Customer Name:</b> {{ $order->customer->code }} -
+                                {{ $order->customer->name }}<br />
                                 <b>Address:</b> <span class="ion-ios-contact-outline"></span>
                                 {{ $order->customer->address }}<br>
                                 <b>Phone:</b> <span class="ion-android-phone-portrait"></span>
@@ -98,9 +101,9 @@
                         </div>
                         <div class="col-sm-3 invoice-col">
                             <div style="color:aliceblue;">
-                                {{--@php
+                                {{-- @php
                                     $uc = substr($order->reference, 0, 6) . substr($order->reference, 6, 10) + 3000;
-                                @endphp--}}
+                                @endphp --}}
                                 {{ QrCode::size(70)->generate($order->total) }}<br />
                                 <span style="font-size:28px;margin-top:-5px">
                                     {{ $order->payment_mode }} Sales
@@ -109,7 +112,8 @@
                         </div>
                         <div class="col-sm-4">
                             <b>Invoice No:</b> {{ $order->reference }}<br>
-                            <b>Date and Time: {{ \Carbon\Carbon::parse($order->order_date)->toFormattedDateString() }}</b><br>
+                            <b>Date and Time:
+                                {{ \Carbon\Carbon::parse($order->order_date)->toFormattedDateString() }}</b><br>
                             <b>Prepared By</b> <span class="ion-card"></span> {{ $order->sold->name }}<br />
                             <b>Printed By <span class="ion-printer"></span>
                                 &nbsp;&nbsp;{{ Auth::user()->name }}</span><span>
@@ -154,11 +158,12 @@
                                         </tr>
                                         @php $total += ($order_detail->sold_price * $order_detail->quantity);  @endphp
                                     @endforeach
+                                    
                                 </tbody>
                             </table>
                             <table class="table table-bordered table-condensed">
                                 <tr>
-                                    <td rowspan="6" style="text-align: left;vertical-align: bottom">
+                                    <td rowspan="3" style="text-align: left;vertical-align: bottom">
                                         Balance C/F =
                                         @if ($order->customer->amount()->sum('cr') - $order->customer->amount()->sum('dr') < 0)
                                             &#8358;({{ number_format(abs($order->customer->amount()->sum('cr') - $order->customer->amount()->sum('dr')), 2) }})
@@ -170,14 +175,15 @@
                                     <th style="text-align: right;">
                                         &#8358;{{ number_format($total, 2, '.', ',') }}</th>
                                 </tr>
-                                {{--@if ($order->discount != 0)
+
+                                {{-- @if ($order->discount != 0)
                                     <tr>
                                         <th style="text-align: right">Discount := </th>
                                         <th style="text-align: right;">
                                             &#8358;{{ number_format($order->discount, 2, '.', ',') }}
                                         </th>
                                     </tr>
-                                @endif--}}
+                                @endif --}}
 
                                 <tr>
                                     <td colspan="4">Amoun Paid in Words:
@@ -203,7 +209,7 @@
                                             style='font-size:14px; border-style:none;'></span>
                                         Signature:
                                         _______________________________<br /><br />
-                                        for: {{App\Models\User::UserBranchName()->long_name}}
+                                        for: {{ App\Models\User::UserBranchName()->long_name }}
                                     </td>
                                 </tr>
                             </table>

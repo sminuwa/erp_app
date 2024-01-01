311a9961bc732194f1bc02c3becbeeef990f349b
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-01-01 13:52:31 +0100

    asfafs

--- app/Http/Controllers/StockAdjustmentController.php
+++ app/Http/Controllers/StockAdjustmentController.php
@@ -38,7 +38,7 @@ class StockAdjustmentController extends Controller
 
     public function index(Index $request)
     {
-        auth()->user()->can('stock_adjustments.index');
+        $this->authorize('stock_adjustments.index1');
         \Cart::clear();
         $user = auth()->user();
         $branch = $user->branch;

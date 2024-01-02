a58821cbcb5077871f46a77ed061ee88e7952e01
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-01-02 15:55:19 +0100

    asfafs

--- app/Http/Requests/Customers/Create.php
+++ app/Http/Requests/Customers/Create.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Customers;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\Customer;
 
-class Create extends FormRequest 
+class Create extends FormRequest
 {
 
     /**
@@ -13,9 +13,9 @@ class Create extends FormRequest
      *
      * @return bool
      */
-    public function authorize() 
+    public function authorize()
     {
-        return $this->user()->can('add.customer', Customer::class);
+        return $this->user()->can('customers.create', Customer::class);
     }
 
     /**
@@ -23,7 +23,7 @@ class Create extends FormRequest
      *
      * @return array
      */
-    public function rules() 
+    public function rules()
     {
         return [
 
@@ -38,7 +38,7 @@ class Create extends FormRequest
     public function messages()
     {
         return [
-     
+
         ];
     }
 

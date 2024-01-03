2c042cd66f85979d56a865fe49ae6970fce940c8
unknown <engrabusadik@gmail.com>
2024-01-03 22:47:28 +0800

    Fixed issue with Purchase GRN and Request

---
+++ app/Http/Requests/PurchaseRequest/Index.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\PurchaseRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Purchase;
+
+class Index extends FormRequest 
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() 
+    {
+        return $this->user()->can('purchases.request.index', Purchase::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() 
+    {
+        return [
+
+        ];
+    }
+
+    /**
+    * Get the error messages for the defined validation rules.
+    *
+    * @return array
+    */
+    public function messages()
+    {
+        return [
+     
+        ];
+    }
+
+}
---
+++ app/Http/Requests/PurchaseRequest/StoreRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\PurchaseRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Purchase;
+
+class StoreRequest extends FormRequest
+
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('purchases.store', Purchase::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+			'supplier_id' => 'required|numeric',
+			'invoice' => 'required|max:191',
+			'purchase_date' => 'required|date',
+			'wbno' => 'nullable|max:191',
+			'status' => 'required',
+			'waybill_no' => 'nullable|max:50',
+			'driver_name' => 'nullable|max:100',
+			'location_id' => 'nullable|max:100',
+			'warehouse' => 'nullable|max:100',
+			'vehicle_reg_no' => 'nullable|max:20',
+			'transporter' => 'nullable|max:100',
+			'updated_by' => 'required|numeric',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+
+        ];
+    }
+
+}

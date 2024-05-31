57887bf8bcaa126e2d39b67308ca6f0e41068592
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-05-31 23:58:14 +0100

    asdfad

--- app/Http/Controllers/OpeningBalanceController.php
+++ app/Http/Controllers/OpeningBalanceController.php
@@ -6,6 +6,10 @@ use App\Imports\Upload;
 use App\Models\Customer;
 use App\Models\GeneralAccount;
 use App\Models\GeneralAccountLedger;
+use App\Models\Product;
+use App\Models\StockAdjustment;
+use App\Models\StockAdjustmentDetail;
+use App\Models\Store;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 
@@ -124,44 +128,88 @@ class OpeningBalanceController extends Controller
             $date = $request->date;
             $branch_id = $request->branch_id;
             $rows = $this->getRecordFromExcel(Upload::class, $file);
-            return $rows;
             $title = $rows[0];
-            $code = searchIndex($title, 'code'); // code/account number
-            $debit = searchIndex($title, 'debit'); //serial number
-            $credit = searchIndex($title, 'credit'); //serial number
-            $product_store = $stock_card =  [];
-            $all_codes = $all_products = $all_stores = [];
+            $product = searchIndex($title, 'product'); // code/account number
+            $store = searchIndex($title, 'store'); //serial number
+            $quantity = searchIndex($title, 'quantity'); //serial number
+            $cost = searchIndex($title, 'cost'); //serial number
+            $product_store = $stock_card = $unique_stores = [];
+            $all_products = $all_stores = [];
             foreach ($rows as $key => $value) {
                 if ($key < 1)
                     continue; // skip first and second row
-                $all_codes[]= $value[$code];
-            }
-            $suppliers = Supplier::whereIn('code',$all_codes)->get()->toArray();
-            foreach ($rows as $key => $value) {
-                if ($key < 1)
-                    continue; // skip first and second row
-
-                $supplier = searchForId(trim($value[$code]), $suppliers);
-                if (!$supplier)
+                if (is_null($value[$store]))
                     continue;
-                $records[] = [
-                    'model_id' => $supplier->id,
-                    'model_name' => 'Supplier',
-                    'branch_id' => $branch_id,
-                    'description' => 'OPENING BALANCE',
-                    'reference' => 'OPENINGBALANCE',
-                    'credit' => str_replace(',','',$value[$credit]),
-                    'debit' => str_replace(',','',$value[$debit]),
-                    'date' => $date,
-                    'user_id' => $user->id,
-                    'receipt_no' => 'OPENINGBALANCE',
+                if (is_null($value[$product]))
+                    continue;
+                $all_products[] = $value[$product];
+                $all_stores[]= $value[$store];
+                if(!in_array(trim($value[$store]),$unique_stores))
+                    $unique_stores[] = trim($value[$store]);
+            }
+//            return $unique_stores;
+            $products = Product::select('id','code')->whereIn('code',$all_products)->get()->toArray();
+            $stores = Store::select('id','code')->whereIn('code',$unique_stores)->get()->toArray();
+            foreach($unique_stores as $s){
+                foreach ($rows as $key => $value) {
+                    if ($key < 1)
+                        continue; // skip first and second row
+                    $pr = searchForId(trim($value[$product]), $products);
+                    $st = searchForId(trim($value[$store]), $stores);
+                    if (!$st || !$pr)
+                        continue;
+                    if($value[$store] == $s) {
+                        $records[$s][] = [
+                            'store_id' => $st->id,
+                            'product_id' => $pr->id,
+                            'quantity' => $branch_id,
+                            'cost_price' => $value[$cost],
+                        ];
+                    }
+                }
+            }
+            $stock_adjustments = $stock_adjustment_details = $stock_references = [];
+            foreach($records as $k => $val){
+                $reference = 'STK'.date('Ym').$k.$branch->code;
+                $records[$k] = [
+                    'reference' =>$reference,
+                    'details'=>$val
+                ];
+                $stock_references[] = $reference;
+                $stock_adjustments[] = [
+                    'reference'=>$reference,
+                    'branch_id'=>$branch_id,
+                    'date'=>$date,
+                    'operation'=>'in',
+                    'description' => $branch->code.'Opening Balance',
+                    'status'=>0,
+                    'created_by'=>$user->id
                 ];
 
             }
 
-            if(GeneralAccountLedger::upsert($records,['model_id', 'model_name', 'branch_id'])) {
-                return view('pages.opening-balance.inventory-valuation', compact('records'));
+            if(StockAdjustment::upsert($stock_adjustments, ['reference', 'branch_id'])){
+                $stock_ad = StockAdjustment::select('id', 'reference')->whereIn('reference', $stock_references)->get()->toArray();
+                foreach($records as $k => $val){
+                    $stock_adjust = searchForId(trim($val['reference']), $stock_ad);
+                    foreach($val['details'] as $details){
+                        $stock_adjustment_details[]= [
+                            'stock_adjustment_id' => $stock_adjust->id,
+                            'store_id' => $details['store_id'],
+                            'product_id' => $details['product_id'],
+                            'quantity' => $details['quantity'],
+                            'cost_price' => $details['cost_price'],
+                            'expiry_date' => null,
+                        ];
+                    }
+
+                }
+
+                if(StockAdjustmentDetail::upsert($stock_adjustment_details,['stock_adjustment_id', 'store_id','product_id','quantity'])){
+                    return view('pages.opening-balance.inventory-valuation', compact('records'));
+                }
             }
+
             return back()->with('error', 'Something went wrong');
         }
 

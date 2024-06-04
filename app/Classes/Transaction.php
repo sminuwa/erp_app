074fec9ffe0da741a73f49938c4b7ca7c2829765
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-06-04 17:56:48 +0100

    asdfa

--- app/Classes/Transaction.php
+++ app/Classes/Transaction.php
@@ -1052,7 +1052,7 @@ class Transaction
         $credit_limit = $customer->credit_limit;
         $balance = $customer->runningBalance() - $total_sales;
 
-        return $credit_limit;
+        return $total_sales;
         if ($balance < 0 && $credit_limit <= 1) {
             return false;
         }

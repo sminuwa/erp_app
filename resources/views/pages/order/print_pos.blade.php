daf2578dc390ec784a1e2d4379277a78589ddad2
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-06-05 12:14:28 +0100

    asdfa

--- resources/views/pages/order/print_pos.blade.php
+++ resources/views/pages/order/print_pos.blade.php
@@ -178,10 +178,10 @@
     <table class="items">
         <thead>
             <tr>
-                <th class="heading">Code</th>
-                <th class="heading">Item</th>
-                <th class="heading">Qty</th>
-                <th class="heading">Unit</th>
+                <th class="heading">Product</th>
+{{--                <th class="heading">Item</th>--}}
+                <th class="heading">Qty / Unit</th>
+{{--                <th class="heading">Unit</th>--}}
                 <th class="heading">Store</th>
                 <th class="heading">Unit Price</th>
                 <th class="heading">Sub Total</th>
@@ -191,10 +191,16 @@
         <tbody>
             @foreach ($order_details as $order_detail)
                 <tr>
-                    <td>{{ $order_detail->storeProduct->product->code }}</td>
-                    <td>{{ $order_detail->storeProduct->product->name }}</td>
-                    <td style="text-align:center">{{ $order_detail->quantity }}</td>
-                    <td>{{ $order_detail->unit ?? ''}}</td>
+                    <td>
+                        {{ $order_detail->storeProduct->product->code }}<br>
+                        {{ $order_detail->storeProduct->product->name }}
+                    </td>
+{{--                    <td>{{ $order_detail->storeProduct->product->name }}</td>--}}
+                    <td style="text-align:center">
+                        {{ $order_detail->quantity }}<br>
+                        {{ $order_detail->unit ?? ''}}
+                    </td>
+{{--                    <td>{{ $order_detail->unit ?? ''}}</td>--}}
                     <td>{{ $order_detail->storeProduct->store->code }}</td>
 
                     <td style="text-align:right">&#8358;{{ number_format($order_detail->sold_price, 2) }}

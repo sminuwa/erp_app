0869edf95c542b4ef15ad3b7a496d880f60e0ce7
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-07-01 14:52:17 +0100

    asfa

--- app/Http/Controllers/PurchaseRequestController.php
+++ app/Http/Controllers/PurchaseRequestController.php
@@ -34,12 +34,17 @@ class PurchaseRequestController extends Controller
     public function index(Index $request)
     {
         \Cart::clear();
-        return view('pages.inventories.purchases.request.index', [
-            'records' => PurchaseRequest::select('purchase_requests.*')->orderBy('purchase_requests.id', 'DESC')
+        $records = PurchaseRequest::select('purchase_requests.*')->orderBy('purchase_requests.id', 'DESC')
+            ->with('supplier')
+            ->where('branch_id', 'LIKE', User::userBranchAction())
+            ->take(10)->get();
+        if(auth()->user()->hasAnyPermission(['access-all-purchase-requests'])) {
+            $records = PurchaseRequest::select('purchase_requests.*')->orderBy('purchase_requests.id', 'DESC')
                 ->with('supplier')
-                ->where('branch_id', 'LIKE', User::userBranchAction())
-                ->take(10)->get()
-        ]);
+                ->take(10)->get();
+        }
+//        access-all-purchase-requests
+        return view('pages.inventories.purchases.request.index', ['records' => $records]);
     }
     public function search(Request $request)
     {
@@ -49,6 +54,11 @@ class PurchaseRequestController extends Controller
             ->join('suppliers', 'suppliers.id', 'purchase_requests.supplier_id')
             ->where('purchase_requests.branch_id', 'LIKE', User::userBranchAction())
             ->orderBy('id', 'DESC')->take(10)->get();
+        if(auth()->user()->hasAnyPermission(['access-all-purchase-requests'])) {
+            $records = PurchaseRequest::select('purchase_requests.*')->where('invoice', 'LIKE', "%$search_value%")
+                ->join('suppliers', 'suppliers.id', 'purchase_requests.supplier_id')
+                ->orderBy('id', 'DESC')->take(10)->get();
+        }
         return view('pages.inventories.purchases.request.index', [
             'records' => $records
         ]);

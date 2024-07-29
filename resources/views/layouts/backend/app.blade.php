56154984fcc46e809740dd71423ccbf5b7ebc5d8
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-07-29 15:46:37 +0100

    asdffs

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -173,10 +173,14 @@
             $(this).parent().nextAll("select").prop("disabled", !this.checked);
         });
 
+        var date = new Date();
+        date.setDate( date.getDate() - 3 );
+            alert(date);
+
         $('.datepicker').datepicker({
             autoclose: true,
             todayHighlight: true,
-            startDate: new Date(),
+            startDate: date,
         });
         $('.datepicker2').datepicker({
             autoclose: true,

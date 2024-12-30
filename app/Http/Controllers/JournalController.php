e05ba502196219789078b481738f1ca5fb0078d2
sunusi inuwa <=>
2024-12-30 20:16:41 +0100

    asdfadf

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -138,10 +138,10 @@ class JournalController extends Controller
             $journal->posted_by = auth()->id();
             $account_details = [];
             $debit = $credit = 0;
-            return $journal->items;
+            // return $journal->items;
             foreach ($journal->items as $item) {
-                $debit += intval($item->debit);
-                $credit += intval($item->credit);
+                $debit += $item->debit;
+                $credit += $item->credit;
                 $account_details[] = [
                     'account_id' => $item->account_id,
                     'branch_id' => $item->branch_id,
@@ -152,7 +152,7 @@ class JournalController extends Controller
             }
             
             //return Transaction::journal($account_details, $journal->reference, $journal->date);
-            return $debit.' - '.$credit;
+            // return $debit.' - '.$credit;
             if ((round($debit) != round($credit)))
                 return back()->with('error', 'Cannot post journal. Make sure total debit and credit are equal.');
             //return Transaction::journal($account_details,$journal->reference,$journal->date);

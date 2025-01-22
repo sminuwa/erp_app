1563cd34aefba400a8c390f50d9dada307d392a3
Sunusi M Inuwa <sminuwa@yahoo.com>
2025-01-22 09:31:35 +0100

    adfasdfa

--- app/Http/Controllers/CreditNoteController.php
+++ app/Http/Controllers/CreditNoteController.php
@@ -269,7 +269,6 @@ class CreditNoteController extends Controller
         $reference = $request->reference;
         $order = Order::where('reference', $reference)->first();
         $order_items = OrderDetail::where('order_id', $order->id)->where('status', 1)->get();
-        // return $order_items;
         \Cart::clear();
         foreach ($order_items as $data) {
             $qty = $data->quantity == 0 ? 1 : $data->quantity;
@@ -289,7 +288,6 @@ class CreditNoteController extends Controller
             ]);
         }
         $cart_products = \Cart::getContent();
-        // return $cart_products;
         return view('pages.inventories.credit_notes.load_products', ['cart_products' => $cart_products, 'reference' => $reference, 'order' => $order]);
     }
     public function addToCart(Request $request)

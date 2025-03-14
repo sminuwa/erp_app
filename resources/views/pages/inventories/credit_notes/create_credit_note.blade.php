aedf998e36ef13114e500a4897df3fc0bf13419d
Sunusi M Inuwa <sminuwa@yahoo.com>
2025-03-14 23:52:15 +0100

    asdfas

--- resources/views/pages/inventories/credit_notes/create_credit_note.blade.php
+++ resources/views/pages/inventories/credit_notes/create_credit_note.blade.php
@@ -118,6 +118,19 @@
                     <div class="col-md-8">
                         {{-- <div id="load"></div> --}}
                         <div class="cart-container"></div>
+                        <form action="{{ route('credit.note.store') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="date" class="date" value="" />
+                            <input type="hidden" name="order_id" id="order_id" value="{{ $order->id }}" />
+                            <input type="hidden" name="customer_id" id="customer_id" value="{{ $order->customer_id }}" />
+                            <input name="comment" placeholder="Comment" class="form-control">
+                
+                            <div class="form-group text-right mt-3">
+                                <input type="submit" onclick="$('.date').val($('.date_').val())" class=" btn btn-primary"
+                                    value="Submit" />
+                            </div>
+                
+                        </form>
                         {{-- @if (isset($order) && $order != null)
                             @include('pages.inventories.credit_notes.load_products')
                         @endif --}}
